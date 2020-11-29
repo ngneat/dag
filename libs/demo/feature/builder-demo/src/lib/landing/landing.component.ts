4b5da31fd549db6b65b7addbf7b619035d86f792
Preston Lamb <preston.j.lamb@gmail.com>
2020-11-29 01:41:36 -0700

    feat(demo): adding leader-line
    Adding leader-line package to draw lines between elements
    not quite working, but should be an easy fix

--- libs/demo/feature/builder-demo/src/lib/landing/landing.component.ts
+++ libs/demo/feature/builder-demo/src/lib/landing/landing.component.ts
@@ -1,6 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { DagManagerService, DagModelItem } from '@ngneat/dag';
 import { Observable } from 'rxjs';
+
+declare let LeaderLine: any;
 
 export interface WorkflowItem extends DagModelItem {
   name: string;
@@ -12,10 +21,12 @@ export interface WorkflowItem extends DagModelItem {
   styleUrls: ['./landing.component.scss'],
   providers: [DagManagerService],
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit, AfterViewInit {
+  @ViewChildren('boxes', { read: ElementRef }) boxes: QueryList<ElementRef>;
   private startingItems: WorkflowItem[] = [
     { name: 'Step 1', stepId: 1, parentIds: [0], branchPath: 1 },
   ];
+  private linesArr = [];
   public workflow$: Observable<WorkflowItem[][]>;
 
   constructor(private _dagManager: DagManagerService<WorkflowItem>) {}
@@ -24,6 +35,50 @@ export class LandingComponent implements OnInit {
     this._dagManager.setNextNumber(2);
     this._dagManager.setNewItemsArrayAsDagModel(this.startingItems);
     this.workflow$ = this._dagManager.dagModel$;
+  }
+
+  ngAfterViewInit() {
+    this.boxes.changes.subscribe((list) => {
+      this.removeLines();
+      this.drawLines();
+    });
+    this.drawLines();
+  }
+
+  removeLines() {
+    this.linesArr.forEach((line) => line.remove());
+    this.linesArr = [];
+  }
+
+  drawLines() {
+    const boxItems = this._dagManager.getSingleDimensionalArrayFromModel();
+    console.log('boxItems:', boxItems);
+
+    boxItems.forEach((box: WorkflowItem) => {
+      box.parentIds.forEach((parentId: number) => {
+        if (parentId > 0) {
+          const parent: ElementRef<any> = this.boxes.find(
+            (b: ElementRef) => parentId === +b.nativeElement.children[0].id
+          );
+          const self: ElementRef<any> = this.boxes.find(
+            (b: ElementRef) => +b.nativeElement.children[0].id === box.stepId
+          );
+          if (parent && self) {
+            const line = new LeaderLine(
+              parent.nativeElement,
+              self.nativeElement,
+              {
+                startSocket: 'bottom',
+                endSocket: 'top',
+                endPlug: 'behind',
+              }
+            );
+
+            this.linesArr.push(line);
+          }
+        }
+      });
+    });
   }
 
   addItem({
