42b489f0a0440316b35b5242a92b1afaf61cb3aa
Preston Lamb <preston.j.lamb@gmail.com>
2020-11-29 00:28:54 -0700

    feat(demo): added header component and placed on page
    Added the header component for the navigation and put it on the page

--- apps/ngneat-dag-demo/src/app/app.module.ts
+++ apps/ngneat-dag-demo/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { SharedUiComponentsModule } from '@ngneat-dag/shared/ui/components';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, SharedUiComponentsModule, RouterModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/shared/ui/components/src/lib/shared-ui-components.module.ts
+++ libs/shared/ui/components/src/lib/shared-ui-components.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
 })
 export class SharedUiComponentsModule {}
