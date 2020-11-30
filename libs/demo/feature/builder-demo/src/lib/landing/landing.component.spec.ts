95e98799a0fc0227afe67eea2e856e09d7d8ede4
Preston Lamb <preston.j.lamb@gmail.com>
2020-11-29 23:05:08 -0700

    test: move more tests off the TestBed

--- libs/demo/feature/builder-demo/src/lib/landing/landing.component.spec.ts
+++ libs/demo/feature/builder-demo/src/lib/landing/landing.component.spec.ts
@@ -1,21 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LandingComponent } from './landing.component';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
-  let fixture: ComponentFixture<LandingComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LandingComponent],
-    }).compileComponents();
-  });
+  let mockDagManagerService;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LandingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new LandingComponent(mockDagManagerService);
   });
 
   it('should create', () => {
--- libs/demo/feature/builder-demo/src/lib/node/node.component.spec.ts
+++ libs/demo/feature/builder-demo/src/lib/node/node.component.spec.ts
@@ -1,21 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NodeComponent } from './node.component';
 
 describe('NodeComponent', () => {
   let component: NodeComponent;
-  let fixture: ComponentFixture<NodeComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [NodeComponent],
-    }).compileComponents();
-  });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NodeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new NodeComponent();
   });
 
   it('should create', () => {
